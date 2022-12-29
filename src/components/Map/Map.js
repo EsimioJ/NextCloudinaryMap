4acdf4d705f99e509916276db90b825bc8ec79f0
Jacopo Riccioni <EsimioJ@users.noreply.github.com>
2022-12-29 23:21:41 +0100

    varie

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -5,12 +5,51 @@ import mapboxgl from "mapbox-gl";
 // I typically like to store sensitive things like this
 // in a .env file
 // mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
-console.log("TOKEN", process.env.MAPBOX_TOKEN);
+// console.log("TOKEN", process.env.MAPBOX_TOKEN);
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZXNpbWlvaiIsImEiOiJOUndHS19RIn0.lFcjnUdKjlJOmCjUfrfzSg";
 
 const Map = () => {
   const mapContainer = useRef();
+
+  const geojson = {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        properties: {
+          message: "Ciao Ruby",
+          iconSize: [60, 60],
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [10.898233333333334, 44.147280555555554],
+        },
+      },
+      {
+        type: "Feature",
+        properties: {
+          message: "Bar",
+          iconSize: [50, 50],
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [10.899322222222223, 44.14808611111111],
+        },
+      },
+      {
+        type: "Feature",
+        properties: {
+          message: "Baz",
+          iconSize: [40, 40],
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [10.899291666666667, 44.14808611111111],
+        },
+      },
+    ],
+  };
 
   // this is where all of our map logic is going to live
   // adding the empty dependency array ensures that the map
@@ -28,6 +67,26 @@ const Map = () => {
       center: [10.8751, 44.1422],
       style: "mapbox://styles/esimioj/cl0pq2rr3001u14n04svzewvp", //mapbox://styles/esimioj/cl0pq2rr3001u14n04svzewvp
     });
+
+    // Add markers to the map.
+    for (const marker of geojson.features) {
+      // Create a DOM element for each marker.
+      const el = document.createElement("div");
+      const width = marker.properties.iconSize[0];
+      const height = marker.properties.iconSize[1];
+      el.className = "marker";
+      el.style.backgroundImage = `url(https://placekitten.com/g/${width}/${height}/)`;
+      el.style.width = `${width}px`;
+      el.style.height = `${height}px`;
+      el.style.backgroundSize = "100%";
+
+      el.addEventListener("click", () => {
+        window.alert(marker.properties.message);
+      });
+
+      // Add markers to the map.
+      new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
+    }
 
     map.on("load", () => {
       map.addSource("mapbox-dem", {
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,94 @@ import Button from "@components/Button";
 import styles from "@styles/Home.module.scss";
 import Map from "@components/Map";
 
-export default function Home({ images, nextCursor }) {
+export default function Home() {
   // console.log("images", images);
   // console.log("nextCursor", nextCursor);
 
+  const images = [
+    
+    {
+      id: 'e02db7fe17409b10f9d70ad8b1729435',
+      width: 1000,
+      height: 1333,
+      public_id: 'casoni/IMG_8249_otvxle',
+      lat: `44 deg 8' 53.24" N`,
+      long: `10 deg 53' 57.80" E`
+    },
+    {
+      id: '7bc16f2c8e6549df269f0e1a8b12be5a',
+      width: 1000,
+      height: 1333,
+      public_id: 'casoni/IMG_E8237_uh91du',
+      lat: `44 deg 8' 45.68" N`,
+      long: `10 deg 54' 5.29" E`
+    },
+    {
+      id: 'd5849a8afe72b07c0f32720614c45739',
+      width: 1333,
+      height: 1000,
+      public_id: 'casoni/IMG_8250_zurpjo',
+      lat: `44 deg 8' 53.61" N`,
+      long: `10 deg 53' 57.71" E`
+    },
+    {
+      id: '7ec5557c1db879fa18134bcef305f8f4',
+      width: 1000,
+      height: 1333,
+      public_id: 'casoni/IMG_8252_wvwnug',
+      lat: `44 deg 8' 53.41" N`,
+      long: `10 deg 53' 53.64" E`
+    },
+    {
+      id: '692d510585cb1113873d1e683e8f5337',
+      width: 1000,
+      height: 1333,
+      public_id: 'casoni/IMG_E8122_npwima',
+      lat: `44 deg 9' 6.43" N`,
+      long: `10 deg 54' 6.47" E`
+    },
+    {
+      id: 'a0052e99e8183ee696d94f67a6e1e9ae',
+      width: 1000,
+      height: 1333,
+      public_id: 'casoni/IMG_E8193_klcklc',
+      lat: `44 deg 8' 59.50" N`,
+      long: `10 deg 54' 14.68" E`
+    },
+    {
+      id: 'de0a97d0a35c199354815a3212d0f2c7',
+      width: 1000,
+      height: 1333,
+      public_id: 'casoni/IMG_8247_wgdvva',
+      lat: `44 deg 8' 53.11" N`,
+      long: `10 deg 53' 57.45" E`
+    },
+    {
+      id: '7c4e0243a4465aa1991a58c21bd7d635',
+      width: 1333,
+      height: 1000,
+      public_id: 'casoni/IMG_8243_bl7u74',
+      lat: `44 deg 8' 50.21" N`,
+      long: `10 deg 53' 53.64" E`
+    },
+    {
+      id: 'c39c1ba98fad2b2ffc7406e4ead6221e',
+      width: 2145,
+      height: 1000,
+      public_id: 'casoni/IMG_8251_qtzgzt',
+      lat: `44 deg 8' 53.85" N`,
+      long: `10 deg 53' 57.67" E`
+    },
+    {
+      id: 'fd2d83700d835be7f70d62e26c22ee7d',
+      width: 1000,
+      height: 1333,
+      public_id: 'casoni/IMG_8248_nrnxkq',
+      lat: `44 deg 8' 53.19" N`,
+      long: `10 deg 53' 57.56" E`
+    }
+  ];
+  console.log(images);
   return (
     <Layout>
       <Head>
@@ -32,16 +116,16 @@ export default function Home({ images, nextCursor }) {
           {images.map((image) => {
             return (
               <li key={image.id}>
-                <a href={image.link} rel="noreferrer">
+                <a href={`https://res.cloudinary.com/dxbxbdarb/image/upload/v1668253659/${image.public_id}.jpg`} rel="noreferrer">
                   <div className={styles.imageImage}>
                     <Image
                       width={image.width}
                       height={image.height}
-                      src={image.image}
-                      alt={image.title}
+                      src={`https://res.cloudinary.com/dxbxbdarb/image/upload/v1668253659/${image.public_id}.jpg`}
+                      alt={image.public_id}
                     />
                   </div>
-                  <h3 className={styles.imageTitle}>{image.title}</h3>
+                  <h3 className={styles.imageTitle}>{image.public_id}</h3>
                 </a>
               </li>
             );
@@ -52,33 +136,3 @@ export default function Home({ images, nextCursor }) {
   );
 }
 
-export async function getStaticProps() {
-  const results = await fetch(
-    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`,
-    {
-      headers: {
-        Authorization: `Basic ${Buffer.from(
-          process.env.CLOUDINARY_API_KEY +
-            ":" +
-            process.env.CLOUDINARY_API_SECRET
-        ).toString("base64")}`,
-      },
-    }
-  ).then((r) => r.json());
-  const { resources, next_cursor: nextCursor } = results;
-
-  const images = resources.map((resource) => {
-    const { width, height } = resource;
-    return {
-      id: resource.asset_id,
-      title: resource.public_id,
-      image: resource.secure_url,
-      width,
-      height,
-    };
-  });
-
-  return {
-    props: { images, nextCursor },
-  };
-}
